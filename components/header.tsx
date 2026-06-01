17c3a83de61e01da2245b96c54bdb3550aa3fac0
v0agent <it+v0agent@vercel.com>
2026-06-01 22:22:36 +0000

    feat: update header logo size and image file
    Increased logo height in header component and updated image file

--- components/header.tsx
+++ components/header.tsx
@@ -82,7 +82,7 @@ export function Header() {
             <img 
               src="/images/logo.png" 
               alt="Saint Germain Physical Therapy" 
-              className="h-12 w-auto"
+              className="h-16 w-auto"
             />
           </a>
 
